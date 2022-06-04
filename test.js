f11ac58b7ee06c8787a9b1f4ffabadfb5a51f378
ColinMVan <colin_vanhoveln@outlook.com>
2022-06-04 09:24:51 -0400

    huge changes

--- test.js
+++ test.js
@@ -1,19 +1,22 @@
-const ingredients = document.querySelector("#ingredients");
-const recipes = document.querySelector("#recipes");
-const tabColor = getComputedStyle(document.documentElement).getPropertyValue(
-  "--tabColor"
-);
+const bars = document.querySelector(".bars");
+const closeButton = document.querySelector(".closeButton");
+const list = document.querySelector(".hiddenList");
 
-ingredients.onclick = () => {
-  document.querySelector("#recipeContent").style.display = "none";
-  document.querySelector("#ingredientContent").style.display = "block";
-  recipes.style.border = "none";
-  ingredients.style.borderBottom = `4px solid ${tabColor}`;
-};
+bars.addEventListener("click", () => {
+  bars.style.display = "none";
+  list.style.left = document.body.offsetWidth + 1 - list.offsetWidth + "px";
+});
 
-recipes.onclick = () => {
-  document.querySelector("#recipeContent").style.display = "block";
-  document.querySelector("#ingredientContent").style.display = "none";
-  ingredients.style.border = "none";
-  recipes.style.borderBottom = `4px solid ${tabColor}`;
-};
+closeButton.addEventListener("click", () => {
+  list.style.left = "100%";
+  bars.style.display = "block";
+});
+
+window.addEventListener("resize", () => {
+  list.style.left = "100%";
+  if (document.body.offsetWidth > 600) {
+    bars.style.display = "none";
+  } else if (document.body.offsetWidth <= 600) {
+    bars.style.display = "block";
+  }
+});
